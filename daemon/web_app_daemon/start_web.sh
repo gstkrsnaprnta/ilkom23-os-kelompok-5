5e4c5585d25e4a599e302ac1f9a5aed5995ad02b
GUSTI KRISNA PRANATA <140774014+gstkrsnaprnta@users.noreply.github.com>
2024-10-26 09:03:10 +0800

    Menambahkan Tugas Daemon

--- daemon/web_app_daemon/start_web.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source "/Users/gustikrisnapranata/ILMU KOMPUTER/SEMESTER 3/SISTEM OPERASI/ilkom23-os-kelompok-5/daemon/web_app_daemon/venv/bin/activate"
-python "/Users/gustikrisnapranata/ILMU KOMPUTER/SEMESTER 3/SISTEM OPERASI/ilkom23-os-kelompok-5/daemon/web_app_daemon/app.py"
